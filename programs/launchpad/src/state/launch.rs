e8115c31cd1c92662560a718bb23d536cfb46b6f
brooksjeremy7 <92278656+brooksjeremy7@users.noreply.github.com>
2025-06-24 11:55:22 -0700

    fix: adding initspace to launchstate as well as it's a subfield on the launch struct

--- programs/launchpad/src/state/launch.rs
+++ programs/launchpad/src/state/launch.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy, PartialEq, Eq)]
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy, PartialEq, Eq, InitSpace)]
 pub enum LaunchState {
     Initialized,
     Live,
