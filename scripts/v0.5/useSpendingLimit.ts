12c8867f5336b09a2662285481b850eb03022910
Kollan House <kollanhouse@Kollans-MacBook-Pro.local>
2025-07-28 15:34:28 -0700

    chore: update the checks for spending limit use

--- scripts/v0.5/useSpendingLimit.ts
+++ scripts/v0.5/useSpendingLimit.ts
@@ -8,14 +8,15 @@ const provider = anchor.AnchorProvider.env();
 const payer = provider.wallet["payer"];
 
 const DAO_ADDRESS = new PublicKey("B3AufDZCDtQN8JxZgJ5bSDZaiKCF4vtw7ynN9tuR9pXN");
-const SPENDING_ADDRESS = new PublicKey("5jRqFejxKHWMfR69dbYF2A9TnpnBPjz7iaRQS44imcMi"); // NOTE: This is your wallet address
-const SPENDING_AMOUNT = 10_000; // in USDC
+const SPENDING_AMOUNT = 10_000; // in human readable TOKEN AMOUNT (eg 10,000)
+const MINT = USDC; // NOTE: This is the mint of the token you want to spend
 
-async function main() {
-  if (payer.publicKey.toBase58() !== SPENDING_ADDRESS.toBase58()) {
-    throw new Error("Payer address does not match spending address");
-  }
-
+// This will fetch the multisig, spending limit and the account details.
+// This checks the mint of the spending limit and the mint of the token you want to spend.
+// This checks for the payer being a member of the spending limit.
+// This checks for the token account and creates it if it doesn't exist.
+// This will spend the amount set above or the remaining amount of the spending limit, whichever is less.
+async function main() { 
   const [multisigPDA] = multisig.getMultisigPda({
     createKey: DAO_ADDRESS,
   });
@@ -30,6 +31,16 @@ async function main() {
     spendingLimitPda
   );
 
+  if(spendingLimitAccount.mint.toBase58() !== MINT.toBase58()) {
+    throw new Error("Spending limit mint does not match the mint");
+  }
+
+  const spendingLimitAmount = Number(spendingLimitAccount.amount.toString());
+  const spendingLimitRemaining = Number(spendingLimitAccount.remainingAmount.toString());
+
+  console.log(`Spending limit: ${spendingLimitAmount / 10 ** 6}`);
+  console.log(`Spending limit remaining: ${spendingLimitRemaining / 10 ** 6}`);
+
   // Confirm the payer is a member of the spending limit
   const members = spendingLimitAccount.members.map(member => member.toBase58());
   if (!members.includes(payer.publicKey.toBase58())) {
@@ -38,13 +49,13 @@ async function main() {
 
   // Get the associated token address for the spending token
   const spendingTokenAccount = getAssociatedTokenAddressSync(
-    USDC,
+    MINT,
     payer.publicKey,
     true
   );
 
   // Get the decimals for the token for use when spending
-  const tokenAccountInfo = await provider.connection.getParsedAccountInfo(USDC);
+  const tokenAccountInfo = await provider.connection.getParsedAccountInfo(MINT);
   if (!tokenAccountInfo.value.data || typeof tokenAccountInfo.value.data === 'string' || 'parsed' in tokenAccountInfo.value.data === false) {
     throw new Error("Token account data is not parsed");
   }
@@ -52,7 +63,13 @@ async function main() {
   const spendingTokenDecimals = tokenAccountInfo.value.data.parsed.info.decimals;
 
   // With decimals, convert the amount from human to chain
-  const amountConverted = SPENDING_AMOUNT * (10 ** spendingTokenDecimals);
+  let amountConverted = SPENDING_AMOUNT * (10 ** spendingTokenDecimals);
+
+  // If the spending limit remaining is less than the amount to spend, adjust the amount to spend to the remaining amount
+  if (spendingLimitRemaining < amountConverted) {
+    console.log(`Spending limit remaining is less than the amount to spend. Adjusting from ${SPENDING_AMOUNT} to ${spendingLimitRemaining / 10 ** 6}`);
+    amountConverted = spendingLimitRemaining;
+  }
 
   const ixs = [];
   try {
@@ -61,15 +78,14 @@ async function main() {
     if (!tokenAccountInfo) {
       throw new Error("Token account not found");
     }
-
   } catch (error) {
-    // Create the spending USDC account if it doesn't exist
+    // Create the spending TOKEN account if it doesn't exist
     console.log("Token account not found, creating it");
     const createIx = createAssociatedTokenAccountIdempotentInstruction(
       payer.publicKey,
       spendingTokenAccount,
       payer.publicKey,
-      USDC
+      MINT
     )
     ixs.push(createIx);
   }
@@ -79,7 +95,7 @@ async function main() {
     multisigPda: multisigPDA,
     member: payer.publicKey,
     spendingLimit: spendingLimitPda,
-    mint: USDC,
+    mint: MINT,
     vaultIndex: 0, // NOTE: If you have multiple vaults, you can use the vaultIndex to specify which vault to use
     amount: amountConverted,
     decimals: spendingTokenDecimals,
