5139b658ddfeef89838543ecd342844ea0465e11
Kollan House <kollanh@gmail.com>
2025-08-25 13:38:09 -0700

    add finalize scripts for ease of use

---
+++ scripts/v0.5/finalizeAndExecuteProposal.ts
@@ -0,0 +1,54 @@
+import * as anchor from "@coral-xyz/anchor";
+import { AmmClient, AutocratClient } from "@metadaoproject/futarchy/v0.5";
+import { PublicKey } from "@solana/web3.js";
+
+const provider = anchor.AnchorProvider.env();
+
+const autocratClient = AutocratClient.createClient({ provider });
+const ammClient = AmmClient.createClient({ provider });
+
+const proposalKey = new PublicKey("Fj5Khdvi4yFzTBsKxXNHw6TTeQwoSRB5DwUWb2qBodks");
+
+async function main() {
+  let proposal = await autocratClient.getProposal(proposalKey);
+  if(!proposal) {
+    console.log('proposal not found');
+    return;
+  }
+
+  if(proposal.state.pending !== undefined) {
+    console.log('proposal is pending attempting finalization');
+
+    try {
+      const crankPassAmm = await ammClient.crankThatTwap(proposal.passAmm);
+      console.log('cranked pass amm', crankPassAmm);
+      const crankFailAmm = await ammClient.crankThatTwap(proposal.failAmm);
+      console.log('cranked fail amm', crankFailAmm);
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
+      const tx = await autocratClient.finalizeProposal(proposalKey);
+      console.log('finalized proposal', tx);
+    } catch (error) {
+      console.log(error);
+    }
+    // Refetch proposal to get the latest state
+    proposal = await autocratClient.getProposal(proposalKey);
+  } else {
+    if(proposal.state.passed === undefined) {
+      console.log('proposal not passed nothing to execute');
+      return;
+    }
+  }
+
+  try {
+    const tx = await autocratClient.finalizeProposal(proposalKey);
+    console.log('finalized proposal', tx);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
