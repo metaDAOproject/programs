0a9034ef874361f18cbe7b5ec8f09c7fb2dfb57e
Pileks <pilex13@gmail.com>
2026-01-05 19:11:58 +0100

    ranger launch script

---
+++ scripts/v0.7/launchRNGR.ts
@@ -0,0 +1,115 @@
+import * as anchor from "@coral-xyz/anchor";
+import {
+  LaunchpadClient,
+  getLaunchAddr,
+  getLaunchSignerAddr,
+} from "@metadaoproject/futarchy/v0.7";
+import { PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import BN from "bn.js";
+import * as token from "@solana/spl-token";
+
+const provider = anchor.AnchorProvider.env();
+const payer = provider.wallet["payer"];
+
+const LAUNCH_AUTHORITY = payer.publicKey;
+
+const TEAM_ADDRESS = new PublicKey(
+  "CHQXGzhweUQ4wrWCta6koL4iQGZNdWQuJmGHxHpRzjBq",
+); // Ranger team address
+
+// Launch details
+const MIN_GOAL = 6_000_000; // 6M USDC
+
+const SPENDING_MEMBERS = [
+  new PublicKey("BXDKN7Bb9s5TPUJksT1HfGgDpYvEGnPwagostU79hAM5"),
+  new PublicKey("5aaFjteWwEeDPZ69j8BmqsGS1KviqzG4V8CMedCkbkhe"),
+];
+const SPENDING_LIMIT = 250_000; // 250k USDC
+
+const PERFORMANCE_PACKAGE_GRANTEE = TEAM_ADDRESS;
+const PERFORMANCE_PACKAGE_TOKEN_AMOUNT = 7_600_000; // 7.6M RNGR
+const PERFORMANCE_PACKAGE_UNLOCK_MONTHS = 18; // 18 months
+
+// Additional carveout details
+const ADDITIONAL_CARVEOUT = 5_125_000; // 5.125M RNGR
+const ADDITIONAL_CARVEOUT_RECIPIENT = new PublicKey(
+  "6awyHMshBGVjJ3ozdSJdyyDE1CTAXUwrpNMaRGMsb4sf",
+); // MetaDAO operational multisig vault
+
+const TOKEN_SEED = "S6Bc84f7fzY6eviV";
+const TOKEN_NAME = "Ranger";
+const TOKEN_SYMBOL = "RNGR";
+const TOKEN_URI =
+  "https://raw.githubusercontent.com/metaDAOproject/futarchy/refs/heads/develop/scripts/assets/RNGR/RNGR.json";
+
+const secondsPerDay = 86_400;
+const numberOfDays = 4;
+const launchDurationSeconds = secondsPerDay * numberOfDays; // 4 days
+
+const launchpad: LaunchpadClient = LaunchpadClient.createClient({ provider });
+
+export const launch = async () => {
+  const lamports = await provider.connection.getMinimumBalanceForRentExemption(
+    token.MINT_SIZE,
+  );
+
+  const TOKEN = await PublicKey.createWithSeed(
+    payer.publicKey,
+    TOKEN_SEED,
+    token.TOKEN_PROGRAM_ID,
+  );
+  console.log("Token address:", TOKEN.toBase58());
+
+  const [launch] = getLaunchAddr(undefined, TOKEN);
+  const [launchSigner] = getLaunchSignerAddr(undefined, launch);
+
+  const tx = new Transaction().add(
+    SystemProgram.createAccountWithSeed({
+      fromPubkey: payer.publicKey,
+      newAccountPubkey: TOKEN,
+      basePubkey: payer.publicKey,
+      seed: TOKEN_SEED,
+      lamports: lamports,
+      space: token.MINT_SIZE,
+      programId: token.TOKEN_PROGRAM_ID,
+    }),
+    token.createInitializeMint2Instruction(TOKEN, 6, launchSigner, null),
+  );
+  tx.recentBlockhash = (
+    await provider.connection.getLatestBlockhash()
+  ).blockhash;
+  tx.feePayer = payer.publicKey;
+  tx.sign(payer);
+
+  const txHash = await provider.connection.sendRawTransaction(tx.serialize());
+  await provider.connection.confirmTransaction(txHash, "confirmed");
+
+  const launchIx = await launchpad
+    .initializeLaunchIx({
+      tokenName: TOKEN_NAME,
+      tokenSymbol: TOKEN_SYMBOL,
+      tokenUri: TOKEN_URI,
+      minimumRaiseAmount: new BN(MIN_GOAL * 10 ** 6),
+      baseMint: TOKEN,
+      monthlySpendingLimitAmount: new BN(SPENDING_LIMIT * 10 ** 6),
+      monthlySpendingLimitMembers: SPENDING_MEMBERS,
+      performancePackageGrantee: PERFORMANCE_PACKAGE_GRANTEE,
+      performancePackageTokenAmount: new BN(
+        PERFORMANCE_PACKAGE_TOKEN_AMOUNT * 10 ** 6,
+      ),
+      monthsUntilInsidersCanUnlock: PERFORMANCE_PACKAGE_UNLOCK_MONTHS,
+      secondsForLaunch: launchDurationSeconds,
+      teamAddress: TEAM_ADDRESS,
+      additionalTokensAmount: ADDITIONAL_CARVEOUT
+        ? new BN(ADDITIONAL_CARVEOUT * 10 ** 6)
+        : undefined,
+      additionalTokensRecipient: ADDITIONAL_CARVEOUT_RECIPIENT,
+      launchAuthority: LAUNCH_AUTHORITY,
+    })
+    .rpc();
+
+  console.log("Launch initialized", launchIx);
+  // await launchpad.startLaunchIx({ launch }).rpc();
+};
+
+launch().catch(console.error);
