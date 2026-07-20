1683638311ffab9b82ba08c30cc080bb7f627aba
Kollan House <kollan@metadao.fi>
2026-07-20 09:47:01 -0700

    init for rip cars

---
+++ scripts/v0.7/rip-cars/constants.ts
@@ -0,0 +1,38 @@
+import { PublicKey } from "@solana/web3.js";
+
+// Token Details
+export const TOKEN_SEED = "TVOzl2TKhXCRVn9U";
+
+export const LAUNCH_AUTHORITY = new PublicKey(
+  "LncRyJVBbek7EFhnd6QNZRTLT9mReKWPGCpsfu5bqJS",
+);
+
+// Team Config Details
+export const TEAM_ADDRESS = new PublicKey(
+  "CnTDWPAEsN5RAgNapTJerDAc45TWavRQ1m3ACMpukYPd",
+); // Rip Cars team squads address
+
+export const LAUNCH_ADDRESS = new PublicKey("");
+
+export const SPENDING_MEMBERS = [
+  new PublicKey("CqK6aBSSycQU3igvptxvhf9YNC1v5EodCuCxuVLPPohh"),
+  new PublicKey("4Huto5Lv8z59tW4EezrYNSvBJhCc9U5bgmR5yH5csFD"),
+];
+// Even without a performance package, defaults need to be set
+export const PERFORMANCE_PACKAGE_GRANTEE = new PublicKey(
+  "7iiE6ncVh5uJuBKw7JcwCjhUT8o2VT2JMeQZ8Tsi5Ckf",
+);
+
+// Amount Details
+export const MIN_GOAL = 250_000; // 250k USDC
+export const SPENDING_LIMIT = 40_000; // 40k USDC
+export const PERFORMANCE_PACKAGE_TOKEN_AMOUNT = 12_900_000; // 12.9M CARS
+export const TOTAL_ALLOCATION = 250_000; // 250k USDC
+export const PERFORMANCE_PACKAGE_UNLOCK_MONTHS = 18; // 18 months
+export const ADDITIONAL_CARVEOUT = 0; // 0 CARS
+export const ADDITIONAL_CARVEOUT_RECIPIENT = new PublicKey(""); // Unused
+
+export const TOKEN_NAME = "Rip Cars";
+export const TOKEN_SYMBOL = "CARS";
+export const TOKEN_URI =
+  "https://raw.githubusercontent.com/metaDAOproject/programs/refs/heads/develop/scripts/assets/CARS/CARS.json";
